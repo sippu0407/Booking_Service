98ef9dd046826a46d5619ee39da50c6085b90205
suryabhan singh <suryabhan.singh.0407@gmail.com>
2024-02-07 15:14:15 +0530

    remove config.json

--- src/index.js
+++ src/index.js
@@ -7,8 +7,12 @@ const app=express();
 const {PORT}=require('./config/serverConfig');
 
 const setServerAndRun=async()=>{
-
+  
   app.listen(PORT,()=>{
+
+    if(process.env.db_sync){
+       db.sequelize.Sync({alter:false});
+    }
     console.log(`server running at ${PORT}`);
   })
 
